bc85a84509d71f77d61a98e5109ad2e98d9550ae
unknown <kevinli60000@gmail.com>
2025-06-03 15:09:51 +0200

    LLM

--- ML/Iris/Iris_GCP/predict_handler.py
+++
@@ -1,8 +0,0 @@
-import joblib
-import numpy as np
-
-def predict(request):
-    model = joblib.load('model.joblib')
-    instances = request.get_json()['instances']
-    predictions = model.predict(np.array(instances))
-    return {'predictions': predictions.tolist()}
